6fc4152ada75c80253c707b260a748edee7ed2dd
Ian Page <radbradmac@Ians-MacBook-Pro.local>
2016-04-02 16:29:13 -0300

    First commit

---
+++ Assets/CoonScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoonScript : MonoBehaviour {
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if(transform.position.z < -5.5){
+
+			if(Input.GetKey(KeyCode.Alpha1)){
+
+				Vector3 pos = new Vector3(0,0, speed * Time.deltaTime);
+				transform.position += pos;
+
+			}
+		}
+		if(transform.position.z > -10){
+
+			if(Input.GetKey(KeyCode.Alpha2)){
+
+				Debug.Log("Nigger");
+				Vector3 pos = new Vector3(0,0, -speed * Time.deltaTime);
+				transform.position += pos;
+
+		}
+
+	}
+}
+}
---
+++ Assets/NIggerScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NIggerScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		float horizontal = Input.GetAxis("Horizontal");
+		float vertical = Input.GetAxis("Vertical");
+
+		Vector3 rotation = new Vector3(horizontal * Time.deltaTime * 100, vertical * Time.deltaTime * 100, 0);
+		transform.Rotate(rotation);
+
+	}
+}

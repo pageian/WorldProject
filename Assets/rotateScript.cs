02aa6ecb00f0111cea98d9e435cc0905a096d72d
Mackenzie Boudreau <mack.boudreau2@gmail.com>
2016-04-03 17:54:46 -0700

    Updated labels, equator

--- Assets/rotateScript.cs
+++ Assets/rotateScript.cs
@@ -6,17 +6,17 @@ public class rotateScript : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 
-		var e = GameObject.Find("E");
-		var k2 = GameObject.Find("K2Title");
-		var k = GameObject.Find("K");
-		var l = GameObject.Find("L");
-		var m = GameObject.Find("M");
-
-		e.SetActive(false);
-		k2.SetActive(false);
-		k.SetActive(false);
-		l.SetActive(false);
-		m.SetActive(false);
+//		var e = GameObject.Find("E");
+//		var k2 = GameObject.Find("K2Title");
+//		var k = GameObject.Find("K");
+//		var l = GameObject.Find("L");
+//		var m = GameObject.Find("M");
+//
+//		e.SetActive(false);
+//		k2.SetActive(false);
+//		k.SetActive(false);
+//		l.SetActive(false);
+//		m.SetActive(false);
 
 	}
 
